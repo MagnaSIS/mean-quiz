5d08ed653bd29c44d3fdfc3c0621dd6ba7dbc814
adrilopezpani <adrilopezpani@gmail.com>
2015-07-28 11:45:20 +0000

    Paso 2 - Vistas, Controladores y Enrutado
    - Uso de ngView para vistas parciales.
    - Uso de múltiples controladores para diferentes vistas.
    - Uso de ngRoute para el enrutado.

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,24 @@
 'use strict';
 
 var quizApp = angular.module('quizApp', [
+    'ngRoute',
     'quizControllers'
 ]);
+
+quizApp.config(function($routeProvider) {
+    $routeProvider
+        .when("/", {
+            controller: "IndexCtrl",
+            controllerAs: "vm",
+            templateUrl: "views/home.html"
+        })
+        .when("/quiz", {
+            controller: "QuizCtrl",
+            controllerAs: "vm",
+            templateUrl: "views/quiz.html"
+        }).when("/quiz/create", {
+            controller: "QuizCreateCtrl",
+            controllerAs: "vm",
+            templateUrl: "views/quiz-create.html"
+        });
+});
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,14 +1,36 @@
 'use strict';
 
+// Datos de prueba
+var quizzes = [{
+    pregunta: '¿Capital de Italia?',
+    respuesta: 'Roma'
+}, {
+    pregunta: '¿Capital de Francia?',
+    respuesta: 'París'
+}];
+
 var quizControllers = angular.module('quizControllers', []);
 
 quizControllers.controller('IndexCtrl', [function() {
     var self = this;
-    self.quizzes = [{
-        pregunta: '¿Capital de Italia?',
-        respuesta: 'Roma'
-    }, {
-        pregunta: '¿Capital de Francia?',
-        respuesta: 'París'
-    }];
+    self.title = 'Quiz: el juego de las preguntas';
+}]);
+
+quizControllers.controller('QuizCtrl', [function() {
+    var self = this;
+    self.quizzes = quizzes;
+
+    self.remove = function(quiz) {
+        var index = quizzes.indexOf(quiz);
+        quizzes.splice(index, 1);
+    };
+}]);
+
+quizControllers.controller('QuizCreateCtrl', [function() {
+    var self = this;
+    self.create = function() {
+        var quiz = self.quiz;
+        quizzes.push(quiz);
+        self.success = 'Pregunta creada correctamente.';
+    };
 }]);
