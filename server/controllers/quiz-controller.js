ca7a6d2d4022de90dbde4f116415045a42b1b79c
adrilopezpani <adrilopezpani@gmail.com>
2015-07-29 15:45:56 +0000

    Paso 7 - Uso de CRUD con Mongoose

--- server/controllers/quiz-controller.js
+++ server/controllers/quiz-controller.js
@@ -1,50 +1,48 @@
 'use strict';
 
-//Datos de prueba
-var quizzes = [{
-    pregunta: '¿Capital de Italia?',
-    respuesta: 'Roma',
-    _id: 0
-}, {
-    pregunta: '¿Capital de Francia?',
-    respuesta: 'París',
-    _id: 1
-}];
-
-var lastId = 1;
+var Quiz = require('../models/quiz');
 
 /* GET /api/quizes
  ** returns an array of quizes OR null if there are no quizes 
  */
 exports.list = function(req, res, next) {
-    res.json(quizzes);
+    var onSuccess = function(quizes) {
+        res.json(quizes);
+    };
+    var onFailure = function(err) {
+        next(err);
+    };
+    Quiz.find().exec().then(onSuccess, onFailure);
 };
 
 /* POST /api/quizes
  ** returns true if the quiz is created OR an error if there is an error
  */
 exports.create = function(req, res, next) {
-    var quiz = req.body;
-    quiz._id = ++lastId;
-    quizzes.push(quiz);
-    res.json({
-        success: true
-    });
+    var onSuccess = function(quiz) {
+        res.json({
+            success: true
+        });
+    };
+    var onFailure = function(err) {
+        next(err);
+    };
+    Quiz.create(req.body).then(onSuccess, onFailure);
 };
 
 /* DELETE /api/quizes/:quizId
  ** returns a true if the quiz is removed OR an error
  */
 exports.remove = function(req, res, next) {
-    var success = false;
-    for (var i = 0; i < quizzes.length; i++) {
-        if (quizzes[i]._id == req.params.quizId) {
-            quizzes.splice(i, 1);
-            success = true;
-            break;
-        }
-    }
-    res.json({
-        success: success
-    });
+    var onSuccess = function() {
+        res.json({
+            success: true
+        });
+    };
+    var onFailure = function(err) {
+        next(err);
+    };
+    Quiz.remove({
+        _id: req.params.quizId
+    }).exec().then(onSuccess, onFailure);
 };
